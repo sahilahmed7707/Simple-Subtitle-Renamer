d6fc04b701494f1ade50a2e8feec788a50303430
sahilahmed7720 <56087505+sahilahmed7720@users.noreply.github.com>
2020-10-01 16:58:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 namespace fs = std::filesystem;
 int main()
 {
-    std::string path = "D:/Movies And Series/Series/Modern Family/New folder";
+    std::cout<<"Enter the path to work in is the form of - D:/Directory/Subdirectory/Modify This Folder Contents"<<std::endl;
+    std::string path{};
+    std::getline(std::cin,path);
+    //std::string path = "D:/Movies And Series/Series/Modern Family/New folder";
     std::vector<std::string> videonames;
     std::vector<fs::directory_entry> subs;
     
