367bd9d366ee614ab5413432b35f1ceb3949ad2d
sahilahmed7720 <56087505+sahilahmed7720@users.noreply.github.com>
2020-10-01 17:19:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,18 @@ int main()
     //std::string path = "D:/Movies And Series/Series/Modern Family/New folder";
     std::vector<std::string> videonames;
     std::vector<fs::directory_entry> subs;
-    
+    std::cout << "Enter video file extension last 3 characters only, Example mp4"<<std::endl;
+    std::string ext1;
+    std::cin >> ext1;
+    for (const auto& entry : fs::directory_iterator(path))
+    {
+        std::stringstream stream;
+        stream << entry.path();
+        std::string st = stream.str();
+        int a = st.size();
+        char e1 = ext1.at(0), e2 = ext1.at(1), e3 = ext1.at(2);
+        if((st.at(a-4)=='m') && (st.at(a - 3) == e2) && (st.at(a-2) == e3))
+        videonames.push_back(st);
+    }
 
 }
